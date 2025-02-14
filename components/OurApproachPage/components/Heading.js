60aedefeb28d62fdac5bb60fafd0c0c73e79bf4f
Shubham Shree <shubhamshree.dev@gmail.com>
2025-02-14 17:52:40 +0530

    Our Approach: alignment fixes

--- components/OurApproachPage/components/Heading.js
+++ components/OurApproachPage/components/Heading.js
@@ -1,9 +1,9 @@
 const Heading = () => {
   return (
     <>
-      <div className="py-14 px-4">
+      <div className="py-7 md:py-14 px-4">
         <div className="max-w-screen-lg mx-auto w-full flex flex-col items-center gap-3">
-          <h1 className="text-3xl sm:text-5xl md:text-6xl leading-tight md:leading-tight tracking-wide">
+          <h1 className="text-2xl sm:text-5xl md:text-6xl leading-tight md:leading-tight tracking-wide">
             POWERING SHIP MANAGEMENT <br className="hidden sm:block" /> WITH THE
             4Ps
           </h1>
