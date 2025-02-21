54c4b1ba6f5b35b1fd765c2bc0c033666cc682f5
Shubham Shree <shubhamshree.dev@gmail.com>
2025-02-21 12:34:13 +0530

    Newsletter content upadted

--- utils/resources.js
+++ utils/resources.js
@@ -1,19 +1,19 @@
 export const findInsideList = [
   {
     title: "Expert Opinions",
-    desc: "Each edition of the Nautilus Newsletter is thoughtfully curated to bring you:",
+    desc: "Perspectives from industry leaders on key maritime topics",
   },
   {
     title: "Insights for Growth",
-    desc: "Strategies, innovations, and best practices to enhance maritime operations and sustainability.",
+    desc: "Strategies, innovations, and best practices to enhance maritime operations and sustainability",
   },
   {
     title: "Latest News and Updates",
-    desc: "Stay informed about the trends and developments shaping the maritime world.",
+    desc: "Stay informed about the trends and developments shaping the maritime world",
   },
   {
     title: "Case Studies",
-    desc: "Real-world examples of innovation and success from Nautilus Shipping.",
+    desc: "Real-world examples of innovation and success from Nautilus Shipping",
   },
 ]
 
