79a1c6cfcbddcff33e89b270cac40598e869c314
Shubham Shree <shubhamshree.dev@gmail.com>
2025-05-07 14:46:59 +0530

    SEO blogs bugs fixes

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
     "Ship Management Company, Vessel Management Services | Nautilus Shipping",
   description: "Delivering excellence in technical and crew management",
   verification: {
-    google: "ximcwDn5nLvnNzaFd5RFKg4kZIsdRpd2fZ5waaTkXZw", // ✅ This is the cleanest way
+    google: "googled87b13e1b76e2421", // ✅ This is the cleanest way
   },
   // alternates: {
   //   canonical: "https://www.nautilusshipping.com", // Add the canonical URL here
