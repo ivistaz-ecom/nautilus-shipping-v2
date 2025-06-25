5e20c1cc75b5cfb28868e917313103e455ceec5f
Shubham Shree <shubhamshree.dev@gmail.com>
2025-06-25 21:29:49 +0530

    News & Insights Page revert back

--- app/news-and-insights/page.js
+++ app/news-and-insights/page.js
@@ -2,64 +2,10 @@ import Header from "@/components/Header/Header"
 import Blogs from "@/components/ResourcesPage/NewsAndInsights/components/Blogs/Blogs"
 import Heading from "@/components/ResourcesPage/NewsAndInsights/components/Heading"
 import LatestNews from "@/components/ResourcesPage/NewsAndInsights/components/LatestNews/LatestNews"
+import NewsAndInsights from "@/components/ResourcesPage/NewsAndInsights/NewsAndInsights"
 import NextSeo from "@/components/Seo/Seo"
 
-export const dynamic = "force-dynamic" // optional if you want fresh data always
-
-async function fetchBlogs() {
-  const getCategoryName = (categoryIds) => {
-    const categoryMap = {
-      100: "Business",
-      107: "Compliance",
-      103: "Crewing",
-      108: "Insights",
-      102: "Shipping",
-      105: "Sustainability",
-      101: "Technical",
-      106: "Technology",
-      104: "Training & Skill Development",
-      99: "Wellbeing & Safety",
-    }
-
-    return categoryIds
-      .map((id) => categoryMap[id] || "Uncategorized")
-      .filter((category) => category !== "Uncategorized")
-  }
-
-  try {
-    const page = 100
-    const response = await fetch(
-      `https://docs.nautilusshipping.com/wp-json/wp/v2/posts?_embed&per_page=${page}`,
-      { cache: "no-store" }
-    )
-    const data = await response.json()
-
-    const formattedBlogs = data
-      .map((post) => {
-        const imageUrl =
-          post._embedded?.["wp:featuredmedia"]?.[0]?.source_url ||
-          "/news-and-insights/image01.png"
-        const formattedDate = new Date(post.date).toISOString()
-
-        return {
-          id: post.id,
-          title: post.title.rendered,
-          slug: post.slug,
-          date: formattedDate,
-          categories: getCategoryName(post.categories),
-          imageUrl,
-          longDesc: post.content.rendered,
-        }
-      })
-      .filter((blog) => blog.id !== 10236)
-
-    return { blogs: formattedBlogs }
-  } catch (err) {
-    return { blogs: [], error: err.message }
-  }
-}
-
-export default async function Page() {
+export default function Page() {
   const seoField = {
     title: "Latest News and Insights | Nautilus Shipping",
     description:
@@ -68,9 +14,6 @@ export default async function Page() {
     metaImage: "/our-approach/img01.png",
     pageType: "WebSite",
   }
-
-  const { blogs, error } = await fetchBlogs()
-
   return (
     <>
       <NextSeo {...seoField} />
@@ -81,7 +24,7 @@ export default async function Page() {
       />
       <Heading />
       <LatestNews />
-      <Blogs blogs={blogs} error={error} />
+      <Blogs />
     </>
   )
 }
