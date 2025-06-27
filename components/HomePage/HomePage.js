f16e0c5aac595699e81bc0a940698187a6902b38
Shubham Shree <shubhamshree.dev@gmail.com>
2025-06-27 16:05:15 +0530

    Optimization fixes

--- components/HomePage/HomePage.js
+++ components/HomePage/HomePage.js
@@ -1,8 +1,8 @@
 "use client"
 
-import { useEffect } from "react"
-import { ParallaxProvider } from "react-scroll-parallax"
-import AOS from "aos"
+// import { useEffect } from "react"
+// import { ParallaxProvider } from "react-scroll-parallax"
+// import AOS from "aos"
 import "aos/dist/aos.css"
 
 import HeroBanner from "./components/HeroBanner/HeroBanner"
